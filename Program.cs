2fd18ec346c3b57fc3f62e4b68458ba3879f6277
Disha Tripathi <dishatripathi91@gmail.com>
2025-08-25 00:55:53 +0530

    Your message about what changed

--- Program.cs
+++ Program.cs
@@ -8,7 +8,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
